051e9cd1fbea7ad43a04e8d2f09a96e22a419b69
= <craig@craigshoemaker.net>
2018-03-01 09:34:14 -0500

    clean up variable names

--- modules/validationRules/quickstartRules.spec.js
+++ modules/validationRules/quickstartRules.spec.js
@@ -1,6 +1,6 @@
-const quickstartRules = require('./quickstartRules');
+const rules = require('./quickstartRules');
 
-const validQuickstart = `
+const validInpt = `
 # Quickstart: Sample quickstart
 
 To complete this quickstart, you need an [Azure subscription](https://azure.microsoft.com/free/?WT.mc_id=A261C142F)
@@ -12,18 +12,18 @@ this is a test
 
 ## Next steps`;
 
-describe('quickstart-rules => ', () => {
+describe('quickstartRules => ', () => {
 
     describe('passes: ', () => {
 
         it('valid document passes all rules', () => {
-            const result = quickstartRules.apply(validQuickstart);
+            const result = rules.apply(validInpt);
             expect(result.total).toEqual(result.passed);
             expect(result.allPassed).toBe(true);
         });
 
         it('is valid if no link to free account', () => {
-            const result = quickstartRules.apply(validQuickstart.replace('azure.microsoft.com/free', ''));
+            const result = rules.apply(validInpt.replace('azure.microsoft.com/free', ''));
             expect(result.total).toEqual(result.passed);
             expect(result.allPassed).toBe(true);
         });
@@ -32,32 +32,32 @@ describe('quickstart-rules => ', () => {
     describe('fails: ', () => {
 
         it('title has a lowercase "q" in "Quickstart"', () => {
-            const invalid = validQuickstart.replace('Quickstart', 'quickstart');
-            const results = quickstartRules.apply(invalid);
+            const invalid = validInpt.replace('Quickstart', 'quickstart');
+            const results = rules.apply(invalid);
             expect(results.brokenRules.includes('Required text in H1: "Quickstart: "')).toBe(true);
         });
 
         it('title has no space after "Quickstart:"', () => {
-            const invalid = validQuickstart.replace('Quickstart: ', 'Quickstart:');
-            const results = quickstartRules.apply(invalid);
+            const invalid = validInpt.replace('Quickstart: ', 'Quickstart:');
+            const results = rules.apply(invalid);
             expect(results.brokenRules.includes('Required text in H1: "Quickstart: "')).toBe(true);
         });
 
         it('"Next steps" comes before "Clean up resources"', () => {
-            const invalid = validQuickstart.replace('# Clean up resources', '# Next steps # Clean up resources');
-            const results = quickstartRules.apply(invalid);
+            const invalid = validInpt.replace('# Clean up resources', '# Next steps # Clean up resources');
+            const results = rules.apply(invalid);
             expect(results.brokenRules.includes('"Clean up resouces" comes before "Next steps"')).toBe(true);
         });
         
         it('missing required "Next steps" section', () => {
-            const invalid = validQuickstart.replace('Next steps', '');
-            const results = quickstartRules.apply(invalid);
+            const invalid = validInpt.replace('Next steps', '');
+            const results = rules.apply(invalid);
             expect(results.brokenRules.includes('Required section: "Next steps"')).toBe(true);
         });
 
         it('free account link comes after first H2', () => {
-            const invalid = validQuickstart.replace('# Quickstart: Sample quickstart', '# Quickstart: Sample quickstart\n## Download sample application');
-            const results = quickstartRules.apply(invalid);
+            const invalid = validInpt.replace('# Quickstart: Sample quickstart', '# Quickstart: Sample quickstart\n## Download sample application');
+            const results = rules.apply(invalid);
             expect(results.allPassed).toBe(false);
         });
     });
