2679d47c8bf30e8951f07be2308430541356b890
= <craig@craigshoemaker.net>
2018-03-02 09:23:31 -0500

    add src param

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const rules = require('./modules/validationRules');
 const renderer = require('./modules/renderer');
 const args = require('yargs').argv;
-const input = '';
+const fs = require('fs');
+
+let input = '';
+
+args.src = "C:\\Users\\cshoe\\Documents\\data\\docs\\azure-docs-pr\\articles\\storage\\blobs\\storage-quickstart-blobs-nodejs.md";
+args.type = 'quickstart';
+args.output = 'string';
+
+if(args.src) {
+    input = fs.readFileSync(args.src, 'utf8');
+}
 
 const response = rules.apply(input, args.type);
 
